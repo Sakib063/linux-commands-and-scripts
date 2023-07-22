1ac7f4082b0cdbf8864820fae1ea85770e13a59d
wendigo <saadmansakib275@gmail.com>
2023-07-22 23:53:36 +0600

    Linux scripts

---
+++ shell scripts/one.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+    # echo 'hello world'
+    # echo 'see the file in the current directory'
+    # echo $PATH
+    # text=5
+    # echo $text
+
+
+    # echo -n "Enter your name:"
+    # read name
+    # echo $name
+
+
+    # echo "Enter 0 or 1"
+    # read n 
+    # if [ $n -eq 1 ] ; then
+    # echo "Number one Shakib Khan"
+    # elif [ $n -eq 0 ] ; then
+    # echo "Zero theke hero hou"
+    # else 
+    #     echo "vul number disos ken?"
+    # fi 
+
+
+#     echo "Enter first number "
+#         read num1
+#         echo "Enter second number"
+#         read num2
+#         if [ $num1 -gt $num2 ] ; then 
+#         echo "$num1 is greater than $num2"
+#         elif [ $num1 -lt $num2 ] ; then
+#         echo "$num1 is less than $num2"
+#         else
+#         echo "$num1 and $num2 are equal"
+#     fi
+
+# # The source of the error: [: –le: binary operator expected is the fact you are using the unicode version of – instead of the regular -
+# # Note: The same apply for the unicode ” you are using instead of regular "
+
+
+    # echo "how are you?"
+    # read answer
+
+    # case $answer in
+    #     good) echo "Good";;
+    #     bad) echo "What happen fren?";;
+    #     mid) echo "skill isuees";;
+    #     *) echo "invalid"
+    # esac
+
+
+    # for((i=0;i<=50;i++))
+    # do 
+    #     echo $i
+    # done
+
+    # echo "the number of args is $#"
+    #     a=1
+    #     for i in $*
+    #     do
+    #     echo "The $a No arg is $i" a=`expr $a + 1`
+    # done
+
+    # for i in `ls`
+    #     do
+    #     echo $i
+    # done
+
+    # echo "Enter your name:"
+    # read name
+    # echo $name
